a3ea242591bac3bab56405600fcb604317833b4d
Mike Solomon <mike@meeshkan.com>
2023-12-30 23:29:16 +0200

    Bit bigger

--- train.py
+++ train.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     config.step_freq = 100
     config.test_size = 0.1
     config.n_features = 2**2
-    config.n_levels = 2
+    config.n_levels = 2**2
     len_files = len(FILES)
     test_files = FILES[: int(len_files * config.test_size)]
     train_files = FILES[int(len_files * config.test_size) :]
