bf5cb4f82bffbfd3d241b697691de14fa5f6df6b
Mike Solomon <mike@meeshkan.com>
2023-12-31 10:56:28 +0200

    Bigger batch

--- train.py
+++ train.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     )
     config = wandb.config
     config.seed = 42
-    config.batch_size = 2**6
+    config.batch_size = 2**4
     config.validation_split = 0.2
     config.learning_rate = 1e-4
     config.epochs = 15
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     config.stride = 2**8
     config.step_freq = 100
     config.test_size = 0.1
-    config.n_features = 2**5
+    config.n_features = 2**4
     config.n_levels = 2**4
     len_files = len(FILES)
     test_files = FILES[: int(len_files * config.test_size)]
