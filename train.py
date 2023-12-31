fa867e16b17e38dca751e68e4a20eea6f6c178be
Mike Solomon <mike@meeshkan.com>
2023-12-31 21:38:35 +0200

    Smaller window

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     config.validation_split = 0.2
     config.learning_rate = 1e-4
     config.epochs = 15
-    config.window = 2**10
+    config.window = 2**9
     config.stride = 2**8
     config.step_freq = 100
     config.test_size = 0.1
