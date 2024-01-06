f3e5811e5525c72c31d24e4392dc5bbb630c9c53
Mike Solomon <mike@meeshkan.com>
2024-01-06 22:11:45 +0200

    Bigger batch

---
+++ init.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install joe screen libsndfile-dev awscli python3.8-venv -y
+aws s3 cp s3://meeshkan-datasets/unsilenced/ ~/data/ --exclude "*" --include "day[12]/nt1_*" --include "day[12]/67_*" --recursive
+git clone https://github.com/mikesol/jaxfun
+cd jaxfun
+python3 -m venv .venv
+source .venv/bin/activate
+pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install -r requirements-tpu.txt
