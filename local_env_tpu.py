648deb57c17cc2e8e67ef9b8a1aa966fc9a3f88e
Mike Solomon <mike@meeshkan.com>
2024-01-06 23:56:27 +0200

    local env tpu

---
+++ local_env_tpu.py
@@ -0,0 +1,8 @@
+from parallelism import Parallelism
+is_tpu = True
+coordinator_address = 'localhost'
+process_id = 0
+num_processes = 1
+do_manual_parallelism_setup = False
+comet_ml_api_key = '2PhZfwKYatT7CGt6C7hSmft6P'
+parallelism = Parallelism.PMAP
