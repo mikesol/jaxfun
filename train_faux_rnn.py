41d155b81d82b45ec4a2f3a56c955d88f99daa1f
Mike Solomon <mike@meeshkan.com>
2024-01-07 09:20:34 +0200

    Prints sharding

--- train_faux_rnn.py
+++ train_faux_rnn.py
@@ -78,7 +78,7 @@ def checkpoint_walker(ckpt):
     def _cmp(i):
         logging.warning(f"considering {type(i)}")
         if type(i) == type(jnp.ones((1,1))):
-            logging.warning(f"info for type {i.is_fully_addressable} {i.is_fully_replicated}")
+            logging.warning(f"info for type {i.is_fully_addressable} {i.is_fully_replicated} {i.sharding}")
         try:
             o = orbax.checkpoint.utils.fully_replicated_host_local_array_to_global_array(i)
             logging.warning("found a fully replicated local array")
