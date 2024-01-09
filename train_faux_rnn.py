94bbfad0b66d17a4f6ee3769d99505af891b7497
Mike Solomon <mike@meeshkan.com>
2024-01-09 06:37:09 +0200

    Dataful mesh

--- train_faux_rnn.py
+++ train_faux_rnn.py
@@ -277,8 +277,8 @@ if __name__ == "__main__":
     _config["inner_skip"] = True
     # _config["shift"] = 2**4
     # _config["dilation"] = 2**0
-    _config["mesh_x"] = 2
-    _config["mesh_y"] = device_len // _config["mesh_x"]
+    _config["mesh_x"] = device_len
+    _config["mesh_y"] = 1
     _config["loss_fn"] = LossFn.LOGCOSH
     ###
     _config["gen_barrier"] = 0.001
