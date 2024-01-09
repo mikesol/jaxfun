e47836a37e3d0c32ca980c80d3eab88ca20476f3
Mike Solomon <mike@meeshkan.com>
2024-01-09 07:25:16 +0200

    Bigger batch

--- train_faux_rnn.py
+++ train_faux_rnn.py
@@ -262,7 +262,7 @@ if __name__ == "__main__":
     # cnn
     _config["seed"] = 42
     _config["inference_artifacts_per_batch_per_epoch"] = 2**2
-    _config["batch_size"] = 2**5
+    _config["batch_size"] = 2**7
     _config["validation_split"] = 0.2
     _config["learning_rate"] = 1e-4
     _config["epochs"] = 2**7
