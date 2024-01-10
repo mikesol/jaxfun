5f3b9f8bf224daee5eeddfd9b7bb49af500b3286
Mike Solomon <mike@meeshkan.com>
2024-01-10 21:34:01 +0200

    Smaller

--- train_tcn.py
+++ train_tcn.py
@@ -268,7 +268,7 @@ if __name__ == "__main__":
     _config["attn_kernel_size"] = 2**5
     _config["heads"] = 2**4
     _config["conv_depth"] = 2**4
-    _config["attn_depth"] = 2**2
+    _config["attn_depth"] = 2**0
     _config["expand_factor"] = 2.0
     _config["positional_encodings"] = True
     _config["kernel_size"] = 7
