7eef1f95c617da44a9b83be99d7c203f9ca798e9
Mike Solomon <mike@meeshkan.com>
2024-01-10 13:20:17 +0200

    New assertion

--- train_rnn.py
+++ train_rnn.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
     _config = {}
     # cnn
     _config["seed"] = 42
-    _config["batch_size"] = 2**5
+    _config["batch_size"] = 2**4
     _config["inference_artifacts_per_batch_per_epoch"] = _config["batch_size"] * 2
     _config["validation_split"] = 0.15
     _config["learning_rate"] = 1e-4
@@ -262,8 +262,8 @@ if __name__ == "__main__":
     _config["inner_skip"] = True
     # _config["shift"] = 2**4
     # _config["dilation"] = 2**0
-    _config["mesh_x"] = device_len
-    _config["mesh_y"] = 1
+    _config["mesh_x"] = 1
+    _config["mesh_y"] = device_len
     _config["loss_fn"] = LossFn.LOGCOSH
     ###
     run.log_parameters(_config)
