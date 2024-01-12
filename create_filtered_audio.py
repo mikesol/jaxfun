abd921767e91b5fccb760ae7dabe4f320e9ca243
Mike Solomon <mike@meeshkan.com>
2024-01-12 17:54:19 -0500

    Eagerly makes sigs

--- create_filtered_audio.py
+++ create_filtered_audio.py
@@ -1,26 +1,16 @@
 import numpy as np
 from scipy import signal
-
-# from concurrent.futures import ProcessPoolExecutor
 from scipy.signal import lfilter
 
-# EXECUTOR = ProcessPoolExecutor()
-
-
-def apply_filter(audio, fs, f0, Q):
-    b, a = signal.iirpeak(f0, Q, fs)
-    filtered_audio = lfilter(b, a, audio)
-    return filtered_audio
 
 
 def create_filtered_audio(num_filters, fs, f0_start, f0_end, Qstart, Qend):
     f0_values = np.logspace(np.log10(f0_start), np.log10(f0_end), num_filters)
     Q_values = np.logspace(np.log10(Qstart), np.log10(Qend), num_filters)
+    sigs = [signal.iirpeak(f0, Q, fs) for f0, Q in zip(f0_values, Q_values)]
 
     def _create_filtered_audio(audio):
-        filtered_audios = [
-            apply_filter(audio, fs, f0, Q) for f0, Q in zip(f0_values, Q_values)
-        ]
+        filtered_audios = [lfilter(b, a, audio) for b, a in sigs]
 
         # Stack the filtered audios to create a (n, num_filters) array
         filtered_audio_stack = np.concatenate(filtered_audios, axis=-1)
