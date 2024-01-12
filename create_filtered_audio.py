2dbaa1f82c96450ba11025dbd5b648878cf4b06a
Mike Solomon <mike@meeshkan.com>
2024-01-12 17:02:40 -0500

    Adds mod

---
+++ create_filtered_audio.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy import signal
+from concurrent.futures import ProcessPoolExecutor
+from scipy.signal import lfilter
+
+EXECUTOR = ProcessPoolExecutor()
+
+def apply_filter(params):
+    audio, fs, f0, Q = params
+    b, a = signal.iirpeak(f0, Q, fs)
+    filtered_audio = lfilter(b, a, audio)
+    return filtered_audio
+
+def create_filtered_audio(audio, num_filters, fs, f0_start, f0_end, Qstart, Qend):
+    f0_values = np.logspace(np.log10(f0_start), np.log10(f0_end), num_filters)
+    Q_values = np.logspace(np.log10(Qstart), np.log10(Qend), num_filters)
+    params = [(audio, fs, f0, Q) for f0, Q in zip(f0_values, Q_values)]
+
+    filtered_audios = list(EXECUTOR.map(apply_filter, params))
+
+    # Stack the filtered audios to create a (n, num_filters) array
+    filtered_audio_stack = np.concatenate(filtered_audios, axis=-1)
+
+    return filtered_audio_stack
