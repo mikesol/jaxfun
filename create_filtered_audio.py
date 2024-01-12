8a3f9be0842ac9df6adeb3ec5b1077785f2ab4c7
Mike Solomon <mike@meeshkan.com>
2024-01-12 17:07:57 -0500

    Apply audio

--- create_filtered_audio.py
+++ create_filtered_audio.py
@@ -5,8 +5,7 @@ from scipy.signal import lfilter
 
 # EXECUTOR = ProcessPoolExecutor()
 
-def apply_filter(params):
-    audio, fs, f0, Q = params
+def apply_filter(audio, fs, f0, Q):
     b, a = signal.iirpeak(f0, Q, fs)
     filtered_audio = lfilter(b, a, audio)
     return filtered_audio
@@ -14,7 +13,7 @@ def apply_filter(params):
 def create_filtered_audio(audio, num_filters, fs, f0_start, f0_end, Qstart, Qend):
     f0_values = np.logspace(np.log10(f0_start), np.log10(f0_end), num_filters)
     Q_values = np.logspace(np.log10(Qstart), np.log10(Qend), num_filters)
-    filtered_audios = [apply_filter((audio, fs, f0, Q)) for f0, Q in zip(f0_values, Q_values)]
+    filtered_audios = [apply_filter(audio, fs, f0, Q) for f0, Q in zip(f0_values, Q_values)]
 
     # Stack the filtered audios to create a (n, num_filters) array
     filtered_audio_stack = np.concatenate(filtered_audios, axis=-1)
