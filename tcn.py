1c9226bfe957e8371edccddc82255012a5f69107
Mike Solomon <mike@meeshkan.com>
2024-01-12 07:43:57 -0500

    Adds sidechain

--- tcn.py
+++ tcn.py
@@ -2,26 +2,79 @@ import flax.linen as nn
 import jax
 import jax.numpy as jnp
 from flax.linen import initializers
+import math
+
+
+class Sidechain(nn.Module):
+    channels: int = 2**6
+    kernel_size: int = 7
+    norm_factor: float = 1.0
+
+    @nn.compact
+    def __call__(self, x):
+        batch_size = x.shape[0]
+        weights = self.param(
+            "weights",
+            nn.with_partitioning(initializers.lecun_normal(), (None, "model")),
+            (self.channels, self.channels, self.kernel_size),
+            jnp.float32,
+        )
+        x_ = x
+
+        def do_unfold(x):
+            x = jax.lax.conv_general_dilated_patches(
+                jnp.transpose(x, (0, 2, 1)),
+                filter_shape=(self.kernel_size,),
+                window_strides=(1,),
+                padding=((0, 0),),
+            )
+            x = jnp.transpose(
+                jnp.reshape(x, (batch_size, self.channels, self.kernel_size, -1)),
+                (0, 2, 1, 3),
+            )
+            return x
+
+        x = do_unfold(x)
+
+        # weights == (b, k, c, s)
+        # x = (b, s, c) weights = (x, c, k) w' = (b, x, s, k) w = (b, k, x, s)
+        w = jnp.transpose(
+            jnp.einsum("abc,dcg->adbg", x_[:, -x.shape[3] :, :], weights), (0, 3, 1, 2)
+        )
+        w = nn.tanh(w / self.norm_factor)
+        x = x * w
+        x = jnp.sum(x, axis=1)
+        x = jnp.transpose(x, (0, 2, 1))
+        return x
 
 
 class TCN(nn.Module):
     features: int
     kernel_dilation: int
     kernel_size: int
+    with_sidechain: bool = True
 
     @nn.compact
     def __call__(self, x, train: bool):
         x_ = x
         x = nn.Conv(
             features=self.features,
-            kernel_init= nn.with_partitioning(
+            kernel_init=nn.with_partitioning(
                 initializers.lecun_normal(), (None, "model")
-            ) if x.shape[-1] > 1 else initializers.lecun_normal(),
+            )
+            if x.shape[-1] > 1
+            else initializers.lecun_normal(),
             kernel_dilation=(self.kernel_dilation,),
             kernel_size=(self.kernel_size,),
             padding=((0, 0),),
             use_bias=False,
-        )(x)
+        )(x_)
+        if self.with_sidechain:
+            x += Sidechain(
+                channels=self.features,
+                kernel_size=self.kernel_size,
+                norm_factor=math.sqrt(self.features),
+            )(x)
         x = nn.BatchNorm(use_running_average=not train)(x)
         x = nn.gelu(x)
         x_res = nn.Conv(
