86f2ba8698f439baef5cc0c661df5c0440b67a71
Mike Solomon <mike@meeshkan.com>
2024-01-14 07:26:18 -0500

    Adds order and sidechain

--- mixer.py
+++ mixer.py
@@ -92,17 +92,18 @@ class MultiBiquadCell(nn.Module):
 
 class MultiBiquadCellWithSidechain(nn.Module):
     channels: int
+    order: int
     carry_init: nn.initializers.Initializer = nn.initializers.zeros_init()
     param_dtype: Dtype = jnp.float32
 
     @nn.compact
-    # inputs is (b, c, 3)
+    # inputs is (b, c, o)
     def __call__(self, carry, inputs):
         weights = self.param(
             "weights",
             initializers.lecun_normal(),
             # 5 for biquad filter
-            (self.channels, self.channels, 5),
+            (self.channels, self.channels, self.order * 2 + 1),
             jnp.float32,
         )
         # the sidechain should only see the present timestamp, so we get rid of the kernels
@@ -127,7 +128,7 @@ class MultiBiquadCellWithSidechain(nn.Module):
         self, rng: PRNGKey, input_shape: Tuple[int, ...]
     ) -> Tuple[Array, Array]:
         batch_dims = input_shape[:1]
-        mem_shape = batch_dims + (2, self.channels)
+        mem_shape = batch_dims + (self.order, self.channels)
         c = self.carry_init(rng, mem_shape, self.param_dtype)
         return c
 
@@ -138,6 +139,7 @@ class MultiBiquadCellWithSidechain(nn.Module):
 
 class MultiBiquad(nn.Module):
     channels: int
+    order: int
     carry_init: nn.initializers.Initializer = nn.initializers.zeros_init()
 
     @nn.compact
@@ -145,7 +147,7 @@ class MultiBiquad(nn.Module):
         inputs = jnp.transpose(
             jax.lax.conv_general_dilated_patches(
                 jnp.transpose(inputs, (0, 2, 1)),
-                filter_shape=(3,),
+                filter_shape=(self.order + 1,),
                 window_strides=(1,),
                 padding=((2, 0),),
             ),
@@ -161,6 +163,7 @@ class MultiBiquad(nn.Module):
 
 class MultiBiquadWithSidechain(nn.Module):
     channels: int
+    order: int
     carry_init: nn.initializers.Initializer = nn.initializers.zeros_init()
 
     @nn.compact
@@ -169,20 +172,20 @@ class MultiBiquadWithSidechain(nn.Module):
         inputs = jnp.transpose(
             jax.lax.conv_general_dilated_patches(
                 jnp.transpose(inputs, (0, 2, 1)),
-                filter_shape=(3,),
+                filter_shape=(self.order + 1,),
                 window_strides=(1,),
                 padding=((2, 0),),
             ),
             (0, 2, 1),
         )
         # (b, c, k, s)
-        inputs = jnp.reshape(inputs, (batch_size, self.channels, 3, -1))
+        inputs = jnp.reshape(inputs, (batch_size, self.channels, self.order + 1, -1))
         # (b, c, s, k)
         inputs = jnp.transpose(inputs, (0, 3, 1, 2))
         return jnp.squeeze(
             nn.RNN(
                 MultiBiquadCellWithSidechain(
-                    channels=self.channels, carry_init=self.carry_init
+                    channels=self.channels, carry_init=self.carry_init, order=self.order
                 )
             )(inputs),
             axis=2,
@@ -191,13 +194,14 @@ class MultiBiquadWithSidechain(nn.Module):
 class MixingBoard(nn.Module):
     channels: int
     depth: int
+    order: int
     carry_init: nn.initializers.Initializer = nn.initializers.zeros_init()
 
     @nn.compact
     def __call__(self, x):
-        x = MultiBiquad(channels=self.channels)(x)
+        x = MultiBiquad(channels=self.channels, order=self.order)(x)
         for _ in range(self.depth - 1):
-            x = MultiBiquad(channels=self.channels)(x)
+            x = MultiBiquadWithSidechain(channels=self.channels, order=self.order)(x)
         x = nn.Dense(features=1)(x)
         return x
         
@@ -209,5 +213,5 @@ if __name__ == "__main__":
     # model = MultiBiquadWithSidechain(channels=channels)
     # print(model.tabulate(jax.random.key(0), jnp.ones((2**2, 2**14, channels))))
     channels = 2**10
-    model = MixingBoard(channels=channels, depth=8)
+    model = MixingBoard(channels=channels, order=2, depth=8)
     print(model.tabulate(jax.random.key(0), jnp.ones((2**2, 2**14, 1))))
--- train_mixer.py
+++ train_mixer.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
 
     run = Experiment(
         api_key=local_env.comet_ml_api_key,
-        project_name="jax-tcn-attn",
+        project_name="jax-mixer",
     )
     _config = {}
     # cnn
@@ -257,6 +257,7 @@ if __name__ == "__main__":
     _config["step_freq"] = 2**6
     _config["test_size"] = 0.1
     _config["channels"] = 2**6
+    _config["order"] = 3
     _config["depth"] = 2**4
     _config["mesh_x"] = device_len // 1
     _config["mesh_y"] = 1
