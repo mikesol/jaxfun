2bc28daf8d5e4b2fb007c6795cb200430c1f14a5
Mike Solomon <mike@meeshkan.com>
2024-01-14 21:49:25 -0500

    Maxes out depth

--- train_mixer.py
+++ train_mixer.py
@@ -251,14 +251,14 @@ if __name__ == "__main__":
     )
     _config["learning_rate"] = 1e-4
     _config["epochs"] = 2**7
-    _config["window"] = 2**12
+    _config["window"] = 2**11
     _config["inference_window"] = 2**13
     _config["stride"] = 2**8
     _config["step_freq"] = 2**6
     _config["test_size"] = 0.1
     _config["channels"] = 2**6
-    _config["order"] = 2**3
-    _config["depth"] = 2**4
+    _config["order"] = 2
+    _config["depth"] = 2**5
     _config["mesh_x"] = device_len // 1
     _config["mesh_y"] = 1
     _config["loss_fn"] = LossFn.LOGCOSH
