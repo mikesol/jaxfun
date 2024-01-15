285121a39b9c18f13eb4142480fdbcf021d0fb54
Mike Solomon <mike@meeshkan.com>
2024-01-15 07:56:03 -0500

    Fixes

--- train_tcn.py
+++ train_tcn.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
     _config["attn_kernel_size"] = 2**5  # 2**6
     _config["heads"] = 2**2
     _config["conv_depth"] = tuple(
-        2**n for n in (12, 11, 10, 9, 9, 8, 8, 7, 7)
+        2**n for n in (12, 11, 10, 9, 8, 8, 7, 7)
     )  # 2**3  # 2**4
     _config["attn_depth"] = 2**3  # 2**2  # 2**4
     _config["sidechain_modulo_l"] = 2
