7b82f2716af8c96856e5dea02e0fb3b1d5177f8c
Mike Solomon <mike@meeshkan.com>
2024-01-15 08:20:52 -0500

    One boundary

--- train_tcn.py
+++ train_tcn.py
@@ -272,7 +272,7 @@ if __name__ == "__main__":
     _config["kernel_dilation"] = 2**1
     _config["conv_kernel_size"] = 2**3
     _config["attn_kernel_size"] = 2**5  # 2**6
-    _config["heads"] = 2**4
+    _config["heads"] = 2**3
     _config["conv_depth"] = tuple(
         2**n for n in (11, 11, 10, 10, 9, 9, 8, 8, 7, 7)
     )  # 2**3  # 2**4
