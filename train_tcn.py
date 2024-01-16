8b7cb485d3b30ed5478e432671e4070ddd0d098f
Mike Solomon <mike@meeshkan.com>
2024-01-16 18:03:36 +0200

    Just a bit bigger

--- train_tcn.py
+++ train_tcn.py
@@ -277,7 +277,7 @@ if __name__ == "__main__":
     _config["heads"] = 2**2
     _config["conv_depth"] = tuple(
         (
-          2**n for n in (10,10,9,9,8,8,7,7)
+          2**n for n in (11,10,9,9,8,8,7,7)
         ) 
     )  # 2**3  # 2**4
     _config["attn_depth"] = 2**3
