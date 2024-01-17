bd05ffbf1a9dbef312fa5cae40224f2a26e86cd1
Mike Solomon <mike@meeshkan.com>
2024-01-17 10:42:13 +0200

    Tries really long window

--- train_tcn.py
+++ train_tcn.py
@@ -266,8 +266,8 @@ if __name__ == "__main__":
     _config["validation_split"] = 0.2
     _config["learning_rate"] = 1e-4
     _config["epochs"] = 2**7
-    _config["window"] = 2**11
-    _config["inference_window"] = 2**11
+    _config["window"] = 2**14
+    _config["inference_window"] = 2**14
     _config["stride"] = 2**8
     _config["step_freq"] = 2**6
     _config["test_size"] = 0.1
@@ -275,11 +275,11 @@ if __name__ == "__main__":
     _config["kernel_dilation"] = 2**1
     _config["conv_kernel_size"] = 2**3
     _config["attn_kernel_size"] = 2**5
-    _config["heads"] = 2**2
+    _config["heads"] = 2**3
     _config["conv_depth"] = tuple(
-        (2**n for n in (11, 10, 9, 9, 8, 8, 7, 7))
+        (2**n for n in (11, 11, 10, 10, 9, 9, 8, 8, 7, 7))
     )  # 2**3  # 2**4
-    _config["attn_depth"] = 2**3
+    _config["attn_depth"] = 2**1
     _config["sidechain_modulo_l"] = 2
     _config["sidechain_modulo_r"] = 1
     _config["expand_factor"] = 2.0
