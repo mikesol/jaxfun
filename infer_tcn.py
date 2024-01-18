c5492f3bc49332bdbf927e1a076129eb551533eb
Mike Solomon <mike@meeshkan.com>
2024-01-18 13:17:30 +0200

    zzz

--- infer_tcn.py
+++ infer_tcn.py
@@ -275,5 +275,5 @@ if __name__ == "__main__":
     print("o after concat", o.shape)
     print("o after squeeze", o.shape)
     soundfile.write("/tmp/input.wav", input_, 44100)
-    soundfile.write("/tmp/prediction.wav", o, 44100)
+    soundfile.write("/tmp/prediction.wav", zzz, 44100)
     soundfile.write("/tmp/target.wav", target_, 44100)
