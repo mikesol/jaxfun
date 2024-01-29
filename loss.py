1b1c260c6be5817e3f023d096b35caffd5bcc020
Mike Solomon <mike@meeshkan.com>
2024-01-29 06:30:45 +0200

    Adds long mrstft

--- loss.py
+++ loss.py
@@ -4,11 +4,13 @@ from fade_in import apply_fade_in
 import stft
 import auraloss_jax as auraloss
 
+
 class LossFn(Enum):
     LOGCOSH = 1
     ESR = 2
     LOGCOSH_RANGE = 3
     MULTI_STFT = 4
+    LONG_MULTI_STFT = 5
 
 
 def LogCoshLoss(input, target, a=1.0, eps=1e-8):
@@ -35,13 +37,28 @@ def Loss_fn_to_loss(loss_fn):
         return lambda x, y: LogCoshLoss(apply_fade_in(x), apply_fade_in(y))
     if loss_fn == LossFn.MULTI_STFT:
         return MultiResolutionSTFTLoss
+    if loss_fn == LossFn.LONG_MULTI_STFT:
+        return LongMultiResolutionSTFTLoss
     raise ValueError(f"What function? {loss_fn}")
+
 
 def MultiResolutionSTFTLoss(x, y):
     div_factor = 2
     fft_sizes = [1024, 2048, 512]
     hop_sizes = [120, 240, 50]
     win_lengths = [600, 1200, 240]
+    params = [
+        stft.init_stft_params(x // div_factor, y // div_factor, z // div_factor)
+        for x, y, z in zip(fft_sizes, hop_sizes, win_lengths)
+    ]
+    return auraloss.multi_resolution_stft_loss(params, x, y)
+
+
+def LongMultiResolutionSTFTLoss(x, y):
+    div_factor = 2
+    fft_sizes = [4096, 2048, 1024, 512]
+    hop_sizes = [480, 240, 120, 60]
+    win_lengths = [2400, 1200, 600, 300]
     params = [
         stft.init_stft_params(x // div_factor, y // div_factor, z // div_factor)
         for x, y, z in zip(fft_sizes, hop_sizes, win_lengths)
