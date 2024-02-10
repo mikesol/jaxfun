477cc812e12300deecb356ba3f945ea0fa09d9da
Mike Solomon <mike@meeshkan.com>
2024-02-10 08:27:39 +0200

    Uses sample rate

--- train_pvc.py
+++ train_pvc.py
@@ -184,7 +184,8 @@ def do_inference(state, input, conversion_config: ConversionConfig):
     i_inv = 1.0 / conversion_config.hop_size
     lastval = np.zeros((o.shape[0], o.shape[-1] // 2))
     index = np.zeros((o.shape[0], o.shape[-1] // 2))
-    o = denormalize(o)
+    o = denormalize(o, conversion_config.sample_rate)
+    print("o shape", o.shape)
     o = jax.vmap(
         partial(
             noscbank,
