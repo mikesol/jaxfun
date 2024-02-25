eaea88347062890b38de7529b8fd1dbf8f96ed8c
Mike Solomon <mike@meeshkan.com>
2024-02-25 08:16:51 +0200

    Fixes small bugs

--- data.py
+++ data.py
@@ -120,6 +120,7 @@ def audio_gen_16(pair, window, stride):
             ii = i[start : start + window]
             assert len(ii) == window
             oo = o[start : start + window]
+            print("DTYPES", ii.dtype, oo.dtype)
             yield {
                 "input": ii,
                 "target": oo,
