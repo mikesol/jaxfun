286bf112628691f99d8695f8ea526fad7457f428
Mike Solomon <mike@meeshkan.com>
2024-02-26 11:17:26 +0200

    i32

--- infer_transformer.py
+++ infer_transformer.py
@@ -292,6 +292,7 @@ if __name__ == "__main__":
     assert input_.min() < 0
     input_ = input_ + 32768
     assert input_.min() >= 0
+    print('input for inference', input_.shape, input_.dtype)
     o = jit_do_inference(state, input_, config.window_plus_one - 1)
     audy = np.reshape(o[0], (-1,))
     audy = audy.astype(np.float32) - 32768
